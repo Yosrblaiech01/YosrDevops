6cb14b710f598c1fca8e73dae09c5fe0f4c5d2ff
Yosrblaiech01 <yosrblaiech6@gmail.com>
2025-12-02 21:29:59 +0100

    Remove SpringBootTest annotation from tests
    Removed SpringBootTest annotation from StudentManagementApplicationTests.

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,10 +1,8 @@
 package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
+ 
 class StudentManagementApplicationTests {
 
    @Test
