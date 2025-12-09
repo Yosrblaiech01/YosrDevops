e1aca5efd87c6f15ccda4bf2df09a911a547c4ae
Yosrblaiech01 <yosrblaiech6@gmail.com>
2025-12-09 20:02:10 +0100

    Update StudentServiceTest.java

--- src/test/java/tn/esprit/studentmanagement/service/StudentServiceTest.java
+++ src/test/java/tn/esprit/studentmanagement/service/StudentServiceTest.java
@@ -1,19 +1,22 @@
 package tn.esprit.studentmanagement.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import tn.esprit.studentmanagement.entities.Student;
 import tn.esprit.studentmanagement.repositories.StudentRepository;
 import tn.esprit.studentmanagement.services.StudentService;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
@@ -26,17 +29,26 @@ class StudentServiceTest {
 
     @Test
     void getAllStudents_shouldReturnListFromRepository() {
-        // GIVEN : le repository renvoie une liste de 2 étudiants
         Student s1 = new Student();
         Student s2 = new Student();
 
         when(studentRepository.findAll()).thenReturn(List.of(s1, s2));
 
-        // WHEN : on appelle le service
         List<Student> result = studentService.getAllStudents();
 
-        // THEN : la taille est bien 2 et le repo a été appelé une seule fois
         assertEquals(2, result.size());
         verify(studentRepository).findAll();
+    }
+
+    @Test
+    void getStudentById_shouldReturnStudent_WhenExists() {
+        Student s = new Student();
+
+        when(studentRepository.findById(1L)).thenReturn(Optional.of(s));
+
+        Student result = studentService.getStudentById(1L);
+
+        assertSame(s, result);
+        verify(studentRepository).findById(1L);
     }
 }
